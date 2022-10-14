6850ab5ac6493fa30b041ffb9bfc0596d87a157c
Colin Greybosh <colingreybosh@gmail.com>
2022-10-14 16:16:40 -0400

    Refactor based on CLI modifications

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,32 @@
 mod cli;
 mod mandelbrot;
 
+use core::panic;
+use mandelbrot::{ImageOptions, generate_image};
+
 fn main() {
-    let cli::Args { width, height, iterations, zoom, out } = cli::get_args();
-    let img = mandelbrot::generate_image(
+    let cli::Args {
+        width,
+        height,
+        x_offset,
+        y_offset,
+        zoom,
+        iterations,
+        out_path,
+    } = cli::get_args();
+    let image_options = &ImageOptions {
         width,
         height,
         iterations,
-        f64::from(zoom)
-    );
-    
-    match img.save(&out) {
+        zoom,
+        x_offset,
+        y_offset,
+    };
+
+    let image_buffer = generate_image(image_options, 0xFF);
+    match image_buffer.save(&out_path) {
         Ok(_) => {
-            println!("Successfully saved image to {:#?}.", out.as_os_str());
+            println!("Successfully saved image to {:#?}.", out_path.as_os_str());
         },
         Err(error) => {
             panic!("Failed to save the image: {:#?}", error);
